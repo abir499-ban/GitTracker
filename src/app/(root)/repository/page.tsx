2a7813954a2b8e4fe18058f4de8a4767e267f298
Abir Banerjee <abirbanerjee793@gmail.com>
2024-12-06 22:39:04 +0530

    metrics update

--- src/app/(root)/repository/page.tsx
+++ src/app/(root)/repository/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { getData } from '@/utils/firestore';
 import { defaultGitHubRepository } from '../../../../constants/constant';
-import {GitFork,Star,CircleDot,Cpu} from 'lucide-react'
+import {GitFork,Star,CircleDot,Cpu, Eye} from 'lucide-react'
 
 
 const page = () => {
@@ -27,7 +27,7 @@ const page = () => {
   }, [])
   return (
     <>
-      <h1 className='text-center text-5xl font-bold font-mono'>Repo Details</h1>
+      <h1 className='text-center text-5xl font-bold font-mono'>{repoDetails.name}</h1>
       <p className='mt-9 text-muted-foreground font-mono'>General Information</p>
       <div className='px-4 py-6 grid grid-cols-2 justify-between gap-8'>
         <div className='h-32 border-2 border-solid border-blue rounded-lg box-border shadow-lg flex flex-row gap-10'>
@@ -45,8 +45,8 @@ const page = () => {
         <p className='p-11 font-mono text-xl'>Open Issues : {repoDetails.open_issues_count}</p> </div>
 
         <div  className='h-32 border-2 border-solid border-blue rounded-lg box-border shadow-lg flex flex-row gap-10'>
-        <Cpu color="#25bcd0"  className='w-20 h-20 ml-10 mt-4' />
-        <p className='p-11 font-mono text-xl'>Size : {repoDetails.size} MB</p> </div>
+        <Eye color="#25bcd0"  className='w-20 h-20 ml-10 mt-4' />
+        <p className='p-11 font-mono text-xl'>Watchers : {repoDetails.subscribers_count}</p> </div>
         
       </div>
     </>
